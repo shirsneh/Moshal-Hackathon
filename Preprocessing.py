b67786e3a07781079fdbf644ba2f32c9ffcaa68a
Maya Goldman <94686128+mayyagoldman@users.noreply.github.com>
2022-11-04 01:44:20 +0200

    changed var names

--- Preprocessing.py
+++ Preprocessing.py
@@ -8,15 +8,16 @@ from matplotlib.backends.backend_pdf import PdfPages
 from sklearn.cluster import KMeans
 from spectral import *
 class HSI_processing():
+    delta = None
     shape = None
     num_samples = None
     num_lines = None
     num_bands = None
     HSlayers = [5, 35, 50, 84]
+    num_clusters = [2]
     DataSet = []
     kmeans = None
-    ClusteredPixels = []
-    Segmentations = [2]
+    cluster_mtxs = []
     mean_vecs = []
 
     def load_data(self, header_path):
@@ -46,33 +47,35 @@ class HSI_processing():
         for x in range(0, self.num_lines):
             for y in range(0, self.num_samples):
                 self.DataSet.append(_data.read_pixel(x,y))  # for each X,Y - take all the laywers as vector
-        for Clusters in self.Segmentations:
+        for Clusters in self.num_clusters:
             print(f"Training {Clusters} Clusters | size: {self.shape} | Layer:")
             self.kmeans = KMeans(n_clusters=Clusters)  # create kmean model with number of clusters
             self.kmeans.fit(self.DataSet)  # train the model on the data
             # Check for each x,y position the cluster it belongs
-            ClusteredPixel = np.zeros((self.num_lines, self.num_samples), dtype=int)
+            Clustered_mtx = np.zeros((self.num_lines, self.num_samples), dtype=int)
             for x in range(0, self.num_lines):
                 for y in range(0, self.num_samples):
-                    ClusteredPixel[x, y] = self.kmeans.predict(_data[x, y, :].reshape(1, -1).astype(float))
+                    Clustered_mtx[x, y] = self.kmeans.predict(_data[x, y, :].reshape(1, -1).astype(float))
             fig, ax = plt.subplots(figsize=(10, 7))
             plt.title(
                 f"ImageSize:{self.num_lines}x{self.num_samples} - {self.num_bands} Layers \n AI Algorithm for segmentation "
                 f"\n Image splited to {Clusters} different segmentation")
-            self.ClusteredPixels.append(ClusteredPixel)
+            self.cluster_mtxs.append(Clustered_mtx)
             ax.set(xlabel='Xaxis', ylabel='Yaxis')
-            plt.imshow(ClusteredPixel)
+            plt.imshow(Clustered_mtx)
             plt.show()
 
     def get_cluster_mean(self):
-        for i in range(len(self.Segmentations)):
-            for k in range(self.Segmentations[i]):
-                mask = np.argwhere(self.ClusteredPixels[i] == k)
+        for i in range(len(self.num_clusters)):
+            for k in range(self.num_clusters[i]):
+                mask = np.argwhere(self.cluster_mtxs[i] == k)
                 sum = 0
                 for m in mask:
                     sum += self.DataSet[m[0] * self.num_lines + m[1]]
                 mean = sum / len(mask)
                 self.mean_vecs.append(mean)
+
+    def get_angle_between_mean_vectors_(self):
 
 
 
