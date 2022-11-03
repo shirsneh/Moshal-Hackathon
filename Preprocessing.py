4418ea84abb2d791d2da55a28c1f3024aabdafee
Maya Goldman <94686128+mayyagoldman@users.noreply.github.com>
2022-11-03 12:30:21 +0200

    created preprocessing class

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,17 +1,13 @@
 import scipy.io as sp
 import re
-
 import matplotlib.pyplot as plt
+class preprocessing():
+    _filename_regex = r"data/(.*).mat"
+    _data = []
+    def get_single_HSI(self, filepath):
+        filename = re.search(self.filename_regex, filepath).group(1)
+        HS_dict = sp.loadmat(filepath)
+        HSI = HS_dict[filename]
+        self._data.append(HSI)
 
 
-def get_HSI(filepath):
-    filename_regex = r"data/(.*).mat"
-    filename = re.search(filename_regex, filepath).group(1)
-    HS_dict = sp.loadmat(filepath)
-    HSI = HS_dict[filename]
-    return HSI
-
-
-img = get_HSI('data/salinasA.mat')
-plt.imshow(img[: , : , 80])
-plt.show()
