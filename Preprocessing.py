0f3b9139a8cc6ccf1e033a9f38fb510e98b3e3d4
Maya Goldman <94686128+mayyagoldman@users.noreply.github.com>
2022-11-04 01:57:51 +0200

    completed angle func

--- Preprocessing.py
+++ Preprocessing.py
@@ -7,6 +7,8 @@ import scipy
 from matplotlib.backends.backend_pdf import PdfPages
 from sklearn.cluster import KMeans
 from spectral import *
+
+
 class HSI_processing():
     delta = None
     shape = None
@@ -19,6 +21,9 @@ class HSI_processing():
     kmeans = None
     cluster_mtxs = []
     mean_vecs = []
+    materials = []
+    cluster_dist = []
+    classification_mtx = []
 
     def load_data(self, header_path):
         _image = envi.open(header_path)
@@ -28,25 +33,25 @@ class HSI_processing():
         self.num_samples = _data.shape[1]
         self.num_bands = _data.shape[2]
 
-    def visualize_raw_data(self , header_path):
-            _image = envi.open(header_path)
-            _data = _image.load()
-            for ChosenLayer in self.HSlayers:
-                fig, ax = plt.subplots(figsize=(10, 7))
-                plt.title(
-                    f"ImageSize:{self.num_lines}x{self.num_samples} - {self.num_bands} Layers \n Layer:{ChosenLayer}")
-                ImageNP = np.asarray(_data[:, :, ChosenLayer])  # take the 2D matrix of the chosen layer
-                ax.set(xlabel='Xaxis', ylabel='Yaxis')
-                plt.imshow(ImageNP)  # show it
-                plt.show()
-                print(f"Finish image process  | size: {self.shape} | Layer:{ChosenLayer}")
+    def visualize_raw_data(self, header_path):
+        _image = envi.open(header_path)
+        _data = _image.load()
+        for ChosenLayer in self.HSlayers:
+            fig, ax = plt.subplots(figsize=(10, 7))
+            plt.title(
+                f"ImageSize:{self.num_lines}x{self.num_samples} - {self.num_bands} Layers \n Layer:{ChosenLayer}")
+            ImageNP = np.asarray(_data[:, :, ChosenLayer])  # take the 2D matrix of the chosen layer
+            ax.set(xlabel='Xaxis', ylabel='Yaxis')
+            plt.imshow(ImageNP)  # show it
+            plt.show()
+            print(f"Finish image process  | size: {self.shape} | Layer:{ChosenLayer}")
 
-    def KMeans(self , header_path):
+    def KMeans(self, header_path):
         _image = envi.open(header_path)
         _data = _image.load()
         for x in range(0, self.num_lines):
             for y in range(0, self.num_samples):
-                self.DataSet.append(_data.read_pixel(x,y))  # for each X,Y - take all the laywers as vector
+                self.DataSet.append(_data.read_pixel(x, y))  # for each X,Y - take all the laywers as vector
         for Clusters in self.num_clusters:
             print(f"Training {Clusters} Clusters | size: {self.shape} | Layer:")
             self.kmeans = KMeans(n_clusters=Clusters)  # create kmean model with number of clusters
@@ -76,7 +81,24 @@ class HSI_processing():
                 self.mean_vecs.append(mean)
 
     def get_angle_between_mean_vectors_(self):
+        def angle_between(v1, v2):
+            v1_u = v1 / np.linalg.norm(v1)
+            v2_u = v2 / np.linalg.norm(v2)
+            return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
+        for i in range(len(self.num_clusters)):
+            classification = []
+            for v1 in self.mean_vecs[i]:
+                angles = []
+                for v2 in self.materials:
+                    angle = angle_between(v1, v2)
+                    angles.append(angle)
+                min_angle = np.argmin(angles)
+                if angles[min_angle] >= self.delta:
+                    classification.append(-1)  # unclassified
+                else:
+                    classification.append(min_angle)
+            self.classification_mtx.append(classification)
 
 
 prep = HSI_processing()
