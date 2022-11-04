0aaff611d7802ea9411c8214fb10ab23e8037756
Shir <Shirsneh54@gmail.com>
2022-11-04 06:11:34 +0200

    TargetVector.py fixed

--- TargetVector.py
+++ TargetVector.py
@@ -1,17 +1,13 @@
 import numpy as np
 
-target_filename = 'data/s07_M3t_Olivine_GDS70.a_Fo89_165um_NIC4bbb_RREF.txt'
+target_filename = 'data/s07_M3t_Olivine_GDS70.b_Fo89_115um_BECKb_AREF.txt'
 
 with open(target_filename) as tf:
-    target_list = tf.readlines()[93:]
+    target_list = tf.readlines()[1:226]
     target_arr = []
 
-    for j in target_list:
-        j = j.replace("\n", "")
-
-    for i in range(0, len(target_list) - 1):
-        if 2 * (1 + i) <= len(target_list):
-            target_arr[i] = (int(target_list[i]) + int(target_list[i+1])) / 2
-        target_arr[i] = target_arr[i]*1000
-    print(target_arr)
+    for i in range(0, (len(target_list)//2 -1)):
+        num1 = float(target_list[2*i].replace("\n", ""))
+        num2 = float(target_list[2*i+1].replace("\n", ""))
+        target_arr.append(((num1 +num2 ) / 2)*1000)
 
